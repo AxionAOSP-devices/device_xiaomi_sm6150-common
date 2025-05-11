15d280582a7ee50ca0c4db52c22febc03662565c
ryzenforce990 <ryzenforce990@users.noreply.github.com>
2025-05-11 08:46:57 +0000

    sm6150-common: update Security

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,6 +155,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_xiaomi
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
+# Security
+VENDOR_SECURITY_PATCH := 2025-02-01
+
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
